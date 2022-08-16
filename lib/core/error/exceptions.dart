cfca9f33967f32c8e2054da75c7a2aff923e8b89
mahmoudhamdy123 <m.hamdy@spiritude.co>
2022-08-16 15:28:31 +0200

    Complete data source

--- lib/core/error/exceptions.dart
+++ lib/core/error/exceptions.dart
@@ -6,3 +6,8 @@ class ServerException implements Exception{
   final ErrorMessageModel errorMessageModel;
   const ServerException({required this.errorMessageModel});
 }
+
+class LocalException implements Exception{
+  final String message;
+  const LocalException({required this.message});
+}
